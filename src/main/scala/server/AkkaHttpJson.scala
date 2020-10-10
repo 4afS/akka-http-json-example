11b1fd9e08dbfcc060a7d3947aba378156edb30e
4afS <4afs.h1@gmail.com>
2020-10-11 02:07:02 +0900

    routing

--- src/main/scala/server/AkkaHttpJson.scala
+++ src/main/scala/server/AkkaHttpJson.scala
@@ -1,5 +1,22 @@
 package server
 
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.Http
+
 object AkkaHttpJson {
-  def main(args: Array[String]): Unit = {}
+  case class Person(name: String, age: Int)
+  case class UserAdded(id: String, timestamp: Long)
+
+  implicit val system = ActorSystem(Behaviors.empty, "AkkaHttpJson")
+
+  val route: Route = (path("api" / "user") & post) {
+    complete("Yep, roger that!")
+  }
+
+  def main(args: Array[String]): Unit = {
+    Http().newServerAt("localhost", 8080).bind(route)
+  }
 }
