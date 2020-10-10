76494f89878a4dee66dccc7817033dd95622442f
4afS <4afs.h1@gmail.com>
2020-10-11 02:26:45 +0900

    use jackson

--- src/main/scala/server/AkkaHttpJson.scala
+++ src/main/scala/server/AkkaHttpJson.scala
@@ -5,11 +5,13 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.Http
+import java.util._
 
 import spray.json._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import java.util._
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+
+import de.heikoseeberger.akkahttpjackson.JacksonSupport
 
 case class Person(name: String, age: Int)
 case class UserAdded(id: String, timestamp: Long)
@@ -34,7 +36,26 @@ trait PersonJsonProtocol extends DefaultJsonProtocol {
 //   }
 // }
 
-object AkkaHttpCirce extends FailFastCirceSupport {
+// object AkkaHttpCirce extends FailFastCirceSupport {
+//   import io.circe.generic.auto._
+
+//   implicit val system = ActorSystem(Behaviors.empty, "AkkaHttpJson")
+
+//   val route: Route = (path("api" / "user") & post) {
+//     entity(as[Person]) { person: Person =>
+//       println(person)
+//       complete(
+//         UserAdded(UUID.randomUUID().toString(), System.currentTimeMillis())
+//       )
+//     }
+//   }
+
+//   def main(args: Array[String]): Unit = {
+//     Http().newServerAt("localhost", 8080).bind(route)
+//   }
+// }
+
+object AkkaHttpJackson extends JacksonSupport {
   import io.circe.generic.auto._
 
   implicit val system = ActorSystem(Behaviors.empty, "AkkaHttpJson")
